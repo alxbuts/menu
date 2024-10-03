9fc07861395f4510d85374b7d99f7394229eaabe
Oleksii Buts <alxbuts@gmail.com>
2024-10-03 12:18:05 +0300

    logo support

--- functions.php
+++ functions.php
@@ -2,5 +2,10 @@
 
 function plain_theme_setup() {
     add_theme_support('title-tag');
+
+    add_theme_support('custom-logo', array(
+        'flex-height' => true,
+        'flex-width'  => true,
+    ));
 }
 add_action('after_setup_theme', 'plain_theme_setup');
--- index.php
+++ index.php
@@ -1,27 +1,37 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php wp_title('|', true, 'right'); ?></title>
     <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
 
     <header>
-        <h1><?php bloginfo('name'); ?></h1>
-        <p><?php bloginfo('description'); ?></p>
+        <?php
+        if (has_custom_logo()) {
+            the_custom_logo();
+        } else {
+        ?>
+            <h1><a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a></h1>
+            <p><?php bloginfo('description'); ?></p>
+        <?php
+        }
+        ?>
     </header>
 
     <main>
         <?php
-        if ( have_posts() ) {
-            while ( have_posts() ) {
+        if (have_posts()) {
+            while (have_posts()) {
                 the_post();
-                ?>
+        ?>
                 <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
                 <div><?php the_content(); ?></div>
-                <?php
+        <?php
             }
         } else {
             echo '<p>No posts found.</p>';
@@ -35,4 +45,5 @@
 
     <?php wp_footer(); ?>
 </body>
+
 </html>
